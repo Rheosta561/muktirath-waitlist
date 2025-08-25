04e9cbc919bf2196d7e722d90c6efa6a9e2758ee
Anubhav Mishra <manubhav731@gmail.com>
2025-08-25 19:11:37 +0530

    new apk added

--- Frontend/RavenWaitlist/src/Screens/LandingPage.tsx
+++ Frontend/RavenWaitlist/src/Screens/LandingPage.tsx
@@ -191,7 +191,7 @@ export default function LandingPage() {
 
 <div className="flex flex-col sm:flex-row justify-center items-center text-center text-sm text-muted-foreground mb-4 gap-2 px-4">
  <a
-    href="/assets/raven_v_001.apk"
+    href="/assets/raven_saves_01.apk"
     download={true}
     target="_blank"
     rel="noopener noreferrer"
